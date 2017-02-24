bd5dd886cf9521446c1821ba001f1aa61e9b2e63
Alister <alistercabral@gmail.com>
2017-02-24 17:04:21 +0530

    easy level integrated

--- index.js
+++ index.js
@@ -1,52 +1,91 @@
-$(document).ready(function(){
+var state, board;
 
-	var state = true;
-	
-	$('.game').hide();
+$('.game').hide();
 
-	$('#newgamebut').click(function(){
-		$('.game').show();
-		$('#newgame').hide();
-		startGame();
-	})
-
+$('#newgamebut').click(function(){
+	$('.game').show();
+	$('#newgame').hide();
+	startGame();
 })
 
-
 function strike(el) {
-	var im = state ? 'x':'o';
+	var im = state ? 'o':'x';
 	el.prepend('<img src="images/'+im+'.png" />');
 	el.addClass(im)
+	board[el[0].id] = im;
 	state = !state;
 	checkGrid();
+	if(state)
+		computerTurn();
 };
 
-function checkGrid(){
-	var blankspace = $('.block').filter(function(){
+function getEmptyBlocks(){
+	return $('.block').filter(function(){
 		return ! ($(this).hasClass('x') || $(this).hasClass('o'))
 	})
-	if(blankspace.length == 0)
+}
+
+function getEmptyBoard(){
+	return board.filter(function(val){
+		return !(val == 'x' || val == 'o');
+	})
+}
+
+function checkGrid(){
+	var blankspace = getEmptyBlocks()
+	if(winning('x'))
+		gameOver('x')
+	else if(winning('o'))
+		gameOver('o')
+	else if(blankspace.length == 0)
 		gameOver();
 }
 
-function gameOver(){
-	setTimeout(function(){
-		if(confirm('Game over. Start new game ?')){
+function gameOver(player){
+	setTimeout(function(player){
+		var str
+		if(player)
+			str = player+' won.'
+		else
+			str = 'tie.'
+		if(confirm(str + ' Start new game ?')){
 			$('.block').map(function(){
 				$(this).empty();
+				$(this).unbind('click');
 				$(this).removeClass('x')
 				$(this).removeClass('o')
 			})
 			$('#newgame').show();
 			$('.game').hide();
 		}
-	},300)
+	},300,player)
+}
+
+function computerTurn(){
+	var options = getEmptyBoard()
+	$('#'+options[Math.floor(Math.random()*options.length)]).trigger('click');
 }
 
 function startGame(){
-	state = true;
+	state = false;
+	board = [0,1,2,3,4,5,6,7,8];
 	$('.game > div > div').click(function(){
 		$(this).unbind('click');
 		strike($(this));
 	})
+}
+
+// winning combinations using the board indexies
+function winning(player){
+	return (
+		(board[0] == player && board[1] == player && board[2] == player) ||
+		(board[3] == player && board[4] == player && board[5] == player) ||
+		(board[6] == player && board[7] == player && board[8] == player) ||
+		(board[0] == player && board[3] == player && board[6] == player) ||
+		(board[1] == player && board[4] == player && board[7] == player) ||
+		(board[2] == player && board[5] == player && board[8] == player) ||
+		(board[0] == player && board[4] == player && board[8] == player) ||
+		(board[2] == player && board[4] == player && board[6] == player)
+	)
+		
 }
