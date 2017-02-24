7a5e5c71963bf753317c4bc34aa00f3154ebe1f4
Alister <alistercabral@gmail.com>
2017-02-24 14:54:46 +0530

    removed jquery functions

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-(function( $ ){
-
-	let state = true;
-
-	$.fn.strike = function(el) {
-	  var im = state ? 'x':'o';
-	  $(this).prepend('<img src="images/'+im+'.png" />');
-	  $(this).addClass(im)
-	  state = !state;
-	  return this;
-	}; 
-
-})( jQuery );
-
-
 $(document).ready(function(){
 
-	$('.grid3x3 > div > div').click(function(){
-		$(this).strike();
-		$(this).unbind('click');
-	})
+	startGame();
+
+	var state = true;
+
+	function strike(el) {
+		var im = state ? 'x':'o';
+		el.prepend('<img src="images/'+im+'.png" />');
+		el.addClass(im)
+		state = !state;
+		checkGrid();
+	};
+
+	function checkGrid(){
+		var blankspace = $('.block').filter(function(){
+			return ! ($(this).hasClass('x') || $(this).hasClass('o'))
+		})
+		if(blankspace.length == 0)
+			gameOver();
+	}
+
+	function gameOver(){
+		setTimeout(function(){
+			if(confirm('Game over. Start new game ?')){
+				$('.block').map(function(){
+					$(this).empty();
+					$(this).removeClass('x')
+					$(this).removeClass('o')
+				})
+				startGame();
+			}
+		},500)
+	}
+
+	function startGame(){
+		state = true;
+		$('.grid3x3 > div > div').click(function(){
+			$(this).unbind('click');
+			strike($(this));
+		})
+	}
+
 })
