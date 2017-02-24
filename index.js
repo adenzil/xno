a69d75c3e0e19f30ce728113a74fe13bbcffcace
Alister <alistercabral@gmail.com>
2017-02-24 15:59:03 +0530

    update landing page

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 $(document).ready(function(){
 
-	startGame();
-
 	var state = true;
+	
+	$('.game').hide();
 
-	function strike(el) {
-		var im = state ? 'x':'o';
-		el.prepend('<img src="images/'+im+'.png" />');
-		el.addClass(im)
-		state = !state;
-		checkGrid();
-	};
-
-	function checkGrid(){
-		var blankspace = $('.block').filter(function(){
-			return ! ($(this).hasClass('x') || $(this).hasClass('o'))
-		})
-		if(blankspace.length == 0)
-			gameOver();
-	}
-
-	function gameOver(){
-		setTimeout(function(){
-			if(confirm('Game over. Start new game ?')){
-				$('.block').map(function(){
-					$(this).empty();
-					$(this).removeClass('x')
-					$(this).removeClass('o')
-				})
-				startGame();
-			}
-		},500)
-	}
-
-	function startGame(){
-		state = true;
-		$('.grid3x3 > div > div').click(function(){
-			$(this).unbind('click');
-			strike($(this));
-		})
-	}
+	$('#newgamebut').click(function(){
+		$('.game').show();
+		$('#newgame').hide();
+		startGame();
+	})
 
 })
+
+
+function strike(el) {
+	var im = state ? 'x':'o';
+	el.prepend('<img src="images/'+im+'.png" />');
+	el.addClass(im)
+	state = !state;
+	checkGrid();
+};
+
+function checkGrid(){
+	var blankspace = $('.block').filter(function(){
+		return ! ($(this).hasClass('x') || $(this).hasClass('o'))
+	})
+	if(blankspace.length == 0)
+		gameOver();
+}
+
+function gameOver(){
+	setTimeout(function(){
+		if(confirm('Game over. Start new game ?')){
+			$('.block').map(function(){
+				$(this).empty();
+				$(this).removeClass('x')
+				$(this).removeClass('o')
+			})
+			$('#newgame').show();
+			$('.game').hide();
+		}
+	},300)
+}
+
+function startGame(){
+	state = true;
+	$('.game > div > div').click(function(){
+		$(this).unbind('click');
+		strike($(this));
+	})
+}
